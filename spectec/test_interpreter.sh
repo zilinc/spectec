2ab0ec363503819946fb45244d7eb443fcf7ea67
Zilin Chen <zilin.chen@unswalumni.com>
2025-04-20 17:53:23 +0800

    fix test script

--- spectec/test_interpreter.sh
+++ spectec/test_interpreter.sh
@@ -3,5 +3,11 @@
 for f in ../../spec/test/core/*.wast
 do
   echo "---------- $f -----------"
-  ./watsup spec/wasm-3.0/* -v -l --interpreter $f
+  ./spectec ../specification/wasm-3.0/* -v -l --interpreter $f
+done
+
+for f in ../../spec/test/core/simd/*.wast
+do
+  echo "---------- $f -----------"
+  ./spectec ../specification/wasm-3.0/* -v -l --interpreter $f
 done
