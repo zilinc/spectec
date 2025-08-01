624a7908b3a8a51bf292889fcb2f2665e6909f94
Zilin Chen <zilin.chen@ntu.edu.sg>
2025-08-01 19:01:41 +0800

    more semantic-preserving changes to DL validator

--- spectec/src/backend-animation/valid.ml
+++ spectec/src/backend-animation/valid.ml
@@ -6,7 +6,12 @@ open Util
 open Error
 open Source
 
+
 exception InvalidDL of string
+
+let string_of_error at msg = string_of_region at ^ " DL animation validation error:\n" ^ msg
+let error at msg = raise (InvalidDL (string_of_error at msg))
+let error_pr at msg prem = error at (msg ^ "\n" ^ "In premise: " ^ string_of_prem prem)
 
 (* Helper: collect free variables from expressions and args *)
 let rec free_vars_path (p : path) : Set.t =
@@ -58,31 +63,29 @@ and free_vars_args (args : arg list) : Set.t =
 (* Validate a single premise *)
 let rec validate_prem (known : Set.t) (prem : prem) : Set.t =
   match prem.it with
-  | RulePr (_, _, e) -> raise (InvalidDL "RulePr found: shouldn't happen.")
+  | RulePr (_, _, e) -> error_pr prem.at "RulePr found: shouldn't happen." prem
   | IfPr e ->
     let fvs = free_vars_exp e in
     let unknowns = Set.diff fvs known in
     if not (Set.is_empty unknowns) then
-      raise (InvalidDL ("IfPr uses unknown variables:\n" ^ string_of_prem prem ^ "\n" ^
-                        "  ▹ unknowns: " ^ string_of_varset unknowns));
+      error_pr prem.at ("IfPr uses unknown variables: " ^ string_of_varset unknowns) prem;
     known
   | LetPr (lhs, rhs, vars) ->
     if not (List.length vars = 1) then
-      raise (InvalidDL ("LetPr must have exactly one variable at premise:\n" ^ string_of_prem prem));
+      error_pr prem.at ("LetPr can only bind one variable, but got " ^ string_of_varset (Set.of_list vars)) prem;
     let var = List.hd vars in
     let rhs_fvs = free_vars_exp rhs in
     let unknowns = Set.diff rhs_fvs known in
     if not (Set.is_empty unknowns) then
-      raise (InvalidDL ("LetPr RHS uses unknown variables at premise:\n" ^ string_of_prem prem ^ "\n" ^
-                        "  ▹ unknowns: " ^ string_of_varset unknowns));
+      error_pr rhs.at ("LetPr RHS uses unknown variables: " ^ string_of_varset unknowns) prem;
     (match lhs.it with
       | VarE lhs_var ->
         if lhs_var.it <> var then
-          raise (InvalidDL ("LetPr LHS variable `" ^ lhs_var.it ^ "` doesn't match binder `" ^ var ^ "` at premise:\n" ^
-                            string_of_prem prem))
-      | _ -> raise (InvalidDL ("Let binding LHS must be a variable at premise:\n" ^ string_of_prem prem)));
+          error_pr lhs.at ("LetPr LHS variable `" ^ lhs_var.it ^ "` doesn't match binder `" ^ var ^ "`.") prem
+      | _ -> error_pr lhs.at ("Let binding LHS must be a variable, but got " ^ string_of_exp lhs) prem
+    );
     if Set.mem var known then
-      raise (InvalidDL ("LetPr binder `" ^ var ^ "` already known at premise:\n" ^ string_of_prem prem));
+      error_pr prem.at ("LetPr binder `" ^ var ^ "` already known") prem;
     Set.add var known
   | IterPr (plist, (iter, pairs)) ->
     (* {t <- t*} is bidirectional so only one side should be known *)
@@ -100,10 +103,10 @@ let rec validate_prem (known : Set.t) (prem : prem) : Set.t =
             (match e.it with
             | VarE id' ->
               (new_knowns := Set.add id'.it !new_knowns; true)
-            | _ -> raise (InvalidDL (
-              "Out-flowing iteration binding {x <- e} ill-formed at premise:\n" ^ string_of_prem prem ^ "\n" ^
-              "  ▹ e should be VarE but got: " ^ string_of_exp e
-            )))
+            | _ -> error_pr e.at
+              ("Out-flowing iteration binding {x <- e} ill-formed." ^
+               "e should be VarE but got " ^ string_of_exp e) prem
+            )
           else  (* No-flow. *)
             false
         else
@@ -118,30 +121,16 @@ let rec validate_prem (known : Set.t) (prem : prem) : Set.t =
         if Set.is_empty unknown_l then
           new_knowns := Set.add id.it !new_knowns
         else
-          raise (InvalidDL (
-            "IterN length `" ^ string_of_exp l ^ "` is unknown at premise:\n" ^ string_of_prem prem ^ "\n" ^
-            "  ▹ unknowns: " ^ string_of_varset unknown_l
-          ))
-      | _ -> ());
-    (* Validate iter expression if it’s ListN (exp, _) *)
-    (match iter with
-     | ListN (e, _) ->
-        let fvs = free_vars_exp e in
-        if not (Set.subset fvs known) then
-          raise (InvalidDL (
-            "Iteration expression uses unknown variables: " ^
-            string_of_varset (Set.diff fvs known) ^
-            "\nIn: " ^ string_of_prem prem
-          ))
-     | _ -> ());
+          error_pr l.at ("IterN length `" ^ string_of_exp l ^ "` contains unknowns: " ^ string_of_varset unknown_l) prem
+      | _ -> ()
+    );
     (* Validate body premise *)
     new_knowns := (List.fold_left validate_prem !new_knowns plist);
     let unknowniterids = Set.of_list (List.map (fun (x, _) -> x.it) unknowniters) in
-    if not (Set.subset unknowniterids !new_knowns) then
-      raise (InvalidDL (
-        ("Either the IterVars or their binders must be known at premise:\n" ^ string_of_prem prem ^ "\n" ^
-         "  ▹ unknowns: " ^ string_of_varset (Set.diff unknowniterids !new_knowns))
-      ))
+    let unknowns = Set.diff unknowniterids !new_knowns in
+    if not (Set.is_empty unknowns) then
+      error_pr prem.at
+        ("Either the IterVars or their binders must be known, but got unknowns: " ^ string_of_varset unknowns) prem
     else
       let learnediters = Set.of_list (List.concat_map (fun (x, e) -> Set.elements (free_vars_exp e)) unknowniters) in
       new_knowns := Set.union !new_knowns learnediters;
@@ -149,14 +138,14 @@ let rec validate_prem (known : Set.t) (prem : prem) : Set.t =
   | ElsePr -> known
 
 (* Validate a single rule clause *)
-let validate_clause args e prems : unit =
+let validate_clause at args e prems : unit =
   let initial_known = free_vars_args args in
   let known_after_premises =
     List.fold_left validate_prem initial_known prems
   in
   let ret_fvs = free_vars_exp e in
   if not (Set.subset ret_fvs known_after_premises) then
-    raise (InvalidDL ("Return value uses unknown variables: \n" ^ string_of_varset (Set.diff ret_fvs known_after_premises)))
+    error at ("Return value uses unknown variables: \n" ^ string_of_varset (Set.diff ret_fvs known_after_premises))
 
 (* Validate a full func_def *)
 let validate_func_def (fd : Def.func_def) : string list option =
@@ -165,11 +154,9 @@ let validate_func_def (fd : Def.func_def) : string list option =
     match clause.it with
       | DefD (_, args, e, prems) ->
         match
-          validate_clause args e prems
+          validate_clause clause.at args e prems
         with
-        | exception (InvalidDL msg) ->
-            let msg = msg ^ "\n... of clause:" ^ (string_of_clause fdid clause) ^ "\n" in
-            Some msg
+        | exception (InvalidDL msg) -> Some msg
         | _ -> None
   ) clauses |> Lib.Option.cat_opts_opt
 
@@ -177,7 +164,7 @@ let validate (dl : Def.dl_def list) : string list option =
   let open Def in
   List.map (fun def ->
     match def with
-      | RuleDef   _ -> Some ["RuleDef found: shouldn't happen."]
-      | HelperDef _ -> Some ["HelperDef found: shouldn't happen."]
+      | RuleDef   rd -> Some [string_of_error rd.at "RuleDef found: shouldn't happen."]
+      | HelperDef hd -> Some [string_of_error hd.at "HelperDef found: shouldn't happen."]
       | FuncDef fd -> validate_func_def fd
   ) dl |> Lib.Option.cat_opts_opt |> Option.map List.flatten
