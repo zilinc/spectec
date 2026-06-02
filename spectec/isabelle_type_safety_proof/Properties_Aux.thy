947c12cb2b46fa4f03c25ac5772c4a3d16dcd3fc
Antanas Kalkauskas <a.kalkauskas@gmail.com>
2026-06-02 16:02:12 +0800

    Add the shape of the main inversion lemma that uses Intr_ok as the principle types predicate.

--- spectec/isabelle_type_safety_proof/Properties_Aux.thy
+++ spectec/isabelle_type_safety_proof/Properties_Aux.thy
@@ -21,4 +21,28 @@ lemma b_e_type_empty1[dest]:
     by auto
   done
 
+lemma main_inversion_lemma:
+  assumes "Instrs_ok C [e] ft"
+  shows "\<exists> ft_principal. (Instr_ok C e ft_principal) \<and> (ft_principal <ti: ft)"
+  using assms
+  apply (induction "[e]" "ft" arbitrary:  rule: Instr_ok_Instrs_ok.inducts(2)[where ?P1.0 = "\<lambda> _ _ _. True"])
+                      apply auto
+  subgoal for C t_1_lst t_2_lst t_3_lst
+    sorry
+  subgoal
+    by (metis instr_subtyping_sub_rule instr_subtyping_trans)
+  subgoal
+    by (metis instr_subtyping_trans instr_subtyping_frame_rule)
+  done
+
+lemma
+  assumes "Instrs_ok C [e] ft"
+    "e = (instr_subcase_0 NOP)"
+  shows "(mk_functype (mk_list []) (mk_list []) <ti: ft)"
+  using main_inversion_lemma[OF assms(1)] assms(2)
+  apply auto
+    apply(cases rule: Instr_ok.cases)
+  by auto
+
+
 end
