93c2f7e16970528e2e6f33abc268e4e159d418e1
Andreas Rossberg <rossberg@mpi-sws.org>
2026-06-12 09:18:33 +0200

    [spectec] Enable to run validator as inferrer

--- spectec/src/il/valid.mli
+++ spectec/src/il/valid.mli
@@ -1,4 +1,5 @@
 val valid : Ast.script -> unit (* raises Error.Error *)
+
 val valid_binders : (Env.t -> 'a -> Env.t) -> Env.t -> 'a list -> Env.t
 
 val valid_param : Env.t -> Ast.param -> Env.t
@@ -7,3 +8,21 @@ val valid_typ : Env.t -> Ast.typ -> unit
 val valid_clause : Env.t -> Ast.id -> Ast.param list -> Ast.typ -> Ast.clause -> unit
 
 val valid_params : Env.t -> Ast.param list -> Env.t
+
+val annotate : Ast.script -> unit
+val annotate_lhs_exp : Env.t -> Ast.exp -> Ast.typ option -> Env.t
+val annotate_rhs_exp : Env.t -> Ast.exp -> Ast.typ option -> Env.t
+
+(* (Re)Annotate the AST with the types inferred from running the validator.
+ *
+ * Notes:
+ * - Validation is bidirectional type-checking. If the optional type is given,
+ *   annotation starts off in checking mode, otherwise in inference mode.
+ * - Not all node types can be fully inferred. The types of CaseE and StrE
+ *   have to be pre-populated if in inference position. Same for TupE, unless
+ *   a transparent type with no dependencies is desired.
+ * - In checking position, the expression may contain variables that are not
+ *   bound in the input environment. Their expected type is then recorded in
+ *   the output environment.
+ * - These functions use global side state and hence are not reentrant.
+ *)
